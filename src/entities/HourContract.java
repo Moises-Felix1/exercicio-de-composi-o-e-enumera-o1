43af7a3a2eb34f9195bc52b5975b28581aa007c6
Moises Felix <moises012felix@gmail.com>
2024-02-24 11:19:33 -0300

    Feito os metodos das classes

--- src/entities/HourContract.java
+++ src/entities/HourContract.java
@@ -1,5 +1,48 @@
 package entities;
 
+import java.util.Date;
+
 public class HourContract {
 
+	private Date date;
+	private Double valuePerHour;
+	private Integer hour;
+	
+	public HourContract() {
+		
+	}
+
+	public HourContract(Date date, double valuePerHour, Integer hour) {
+		this.date = date;
+		this.valuePerHour = valuePerHour;
+		hour = hour;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public double getValuePerHour() {
+		return valuePerHour;
+	}
+
+	public void setValuePerHour(double valuePerHour) {
+		this.valuePerHour = valuePerHour;
+	}
+
+	public Integer getHour() {
+		return hour;
+	}
+
+	public void setHour(Integer hour) {
+		hour = hour;
+	}
+	
+	public Double totalValue() {
+		return valuePerHour * hour;
+	}
 }
